2dbf404cdbeb6b2d5c08e889c855d1ec014d32ad
mydoum <a.alonso.giraldo@gmail.com>
2017-06-26 09:50:44 +0200

    Check if the user already exist

--- common/models/contact.js
+++ common/models/contact.js
@@ -3,10 +3,32 @@
 module.exports = function(Contact) {
   Contact.beforeRemote('create', function(ctx, _modelInstance_, next) {
     console.log(ctx.args.data);
-    ctx.args.data['creation-date'] = new Date();
-    if (!ctx.args.data['last-contacted']) {
-      ctx.args.data['last-contacted'] = new Date();
+    Contact.app.models.Contact.findOne({where: {email: ctx.args.data.email}},
+      function(err, user) {
+        if (user)
+          next(new Error('409_already_exist'));
+        else {
+          ctx.args.data['creation-date'] = new Date();
+          if (!ctx.args.data['last-contacted']) {
+            ctx.args.data['last-contacted'] = new Date();
+          }
+          next();
+        }
+      });
+  });
+
+  Contact.afterRemoteError('create', function(context, next) {
+    console.log('context: ', context.args);
+    switch (context.error.message) {
+      case '409_already_exist':
+        context.error.message = 'This contact already exist';
+        context.error.statusCode = 409;
+        break;
     }
+
+    context.error.message = 'The `Contact` instance is not valid. Details: ' +
+      context.error.message;
+    delete context.error.stack;
     next();
   });
 };
